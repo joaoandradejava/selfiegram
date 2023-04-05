f97a684d17b28f5b57d5fc703514834d3be95ea3
joaoandradejava <andradepinheironeto24@hotmail.com>
2023-04-05 02:31:35 -0300

    Primeiro commit

---
+++ src/main/java/com/joaoandradejava/selfiegram/SelfiegramApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandradejava.selfiegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfiegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfiegramApplication.class, args);
+	}
+
+}
